156d374a7f82f74b03b9dfec8412d93207bd7772
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-18 17:51:38 -0600

    implement and setup the fontawesome library dependencies within the project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-red-600">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline text-blue-600">Hello world!</h1>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+/* Set up using Vue 3 */
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
