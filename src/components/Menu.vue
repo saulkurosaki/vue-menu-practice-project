b443561038a60a595edd507a0428d27f16cd0eff
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-18 19:30:44 -0600

    feat(Menu): implement the MenuItem component within the main component along its props

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import menuItems from "@/lib/menuItems";
+import MenuItem from "./MenuItem.vue";
 
 const collapsed = ref(false);
 </script>
@@ -9,15 +10,26 @@ const collapsed = ref(false);
   <div :class="['menu', collapsed ? 'collapsed' : 'expanded']">
     <div class="header">
       <button class="menu-button" @click="collapsed = !collapsed">
-        <font-awesome-icon :icon="['fas', 'bars']" />
+        <font-awesome-icon :icon="['fas-solid', 'bars']" />
       </button>
     </div>
-  </div>
 
-  <div class="profile">
-    <img :src="menuItems.profile.image" alt="" />
-    <div class="profile-name" v-if="!collapsed">
-      {{ menuItems.profile.name }}
+    <div class="profile">
+      <img :src="menuItems.profile.image" alt="" />
+      <div class="profile-name" v-if="!collapsed">
+        {{ menuItems.profile.name }}
+      </div>
+    </div>
+
+    <div class="menu-items">
+      <ul>
+        <MenuItem
+          v-for="menuItem in menuItems.items"
+          :key="menuItem.id"
+          :item="menuItem"
+          :collapsed="collapsed"
+        />
+      </ul>
     </div>
   </div>
 </template>
--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { defineProps } from "vue";
 
-<template></template>
+const props = defineProps(["item", "collapsed"]);
+</script>
+
+<template>
+  <a href="#">
+    <div class="item">
+      <div class="icon">
+        <font-awesome-icon :icon="['fas-solid', props.item.icon]" size="2x" />
+      </div>
+      <div class="title" v-if="!collapsed">{{ props.item.title }}</div>
+    </div>
+  </a>
+</template>
 
 <style scoped></style>
